facf599dc5f04dbebcaca94d3b1dafc7be0ccef3
jerry <jerrylai19990120@gmail.com>
2021-05-20 18:19:27 +0800

    Initial Commit

---
+++ Sticky Notes/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Sticky Notes
+//
+//  Created by Jerry Lai on 2021-05-20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Sticky Notes/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Sticky Notes
+//
+//  Created by Jerry Lai on 2021-05-20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
